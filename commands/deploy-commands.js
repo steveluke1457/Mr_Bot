34faff9530dba73c87fb68a1fde329c8a5dff1df
steveluke1457 <steve.luke1457@gmail.com>
2025-05-25 23:41:56 +0300

    Create deploy-commands.js

---
+++ commands/deploy-commands.js
@@ -0,0 +1,26 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+require('dotenv').config();
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('🛰 Registering slash commands...');
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+    console.log('✅ Slash commands registered.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
